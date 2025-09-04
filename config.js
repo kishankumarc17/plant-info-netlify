7dc03927783d4bd236d8481e381fc51719f48b18
kishankumarc17 <kishankumarc17@gmail.com>
2025-09-04 18:18:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 // Supabase project credentials
 const SUPABASE_URL = "https://ntvcmqvcwkzrgsexpnip.supabase.co"; 
-const SUPABASE_ANON_KEY = "PASTE_YOUR_FULL_ANON_KEY_HERE";  
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50dmNtcXZjd2t6cmdzZXhwbmlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MDk4MDAsImV4cCI6MjA3MjM4NTgwMH0.uVbUY27Lgkj7QGDliGPlWiE3QAhbzG3DiCiPOFYUWrI";  
 
 // Create client
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
