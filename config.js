5b26909acd0723362a514c07265e2ddeb9e5ea85
kishankumarc17 <kishankumarc17@gmail.com>
2025-09-04 16:34:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ const SUPABASE_URL = "https://https://ntvcmqvcwkzrgsexpnip.supabase.co";   // pa
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50dmNtcXZjd2t6cmdzZXhwbmlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MDk4MDAsImV4cCI6MjA3MjM4NTgwMH0.uVbUY27Lgkj7QGDliGPlWiE3QAhbzG3DiCiPOFYUWrI";   // paste your anon key here
 
 // Create Supabase client
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window.supabase.createClient(https://https://ntvcmqvcwkzrgsexpnip.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50dmNtcXZjd2t6cmdzZXhwbmlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MDk4MDAsImV4cCI6MjA3MjM4NTgwMH0.uVbUY27Lgkj7QGDliGPlWiE3QAhbzG3DiCiPOFYUWrI);
